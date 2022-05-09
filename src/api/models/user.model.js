4862e81014005d9bb3ed4f67c3d656da35248c95
Sonola Moyo <sonolamoyosoluwalorun@gmail.com>
2022-05-09 15:59:10 +0100

    Task 05

--- src/api/models/user.model.js
+++ src/api/models/user.model.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const userSchema = mongoose.Schema({
     firstname: {type: String, required: true,min:2, max: 50, trim: true},
     lastname: {type: String, required: true,min:2, max: 50, trim: true},
-    email: {type: String, lowercase: true, min:2, max:255, unique: true},
+    email: {type: String, lowercase: true, min:2, max:255},
     password: {type: String, required: true, min: 8},
     active: {type: Boolean, default: true},
 }, {timestamps: true});
